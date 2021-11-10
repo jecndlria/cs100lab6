960232f36e8b450ce3ccacc759f128281bd2a60d
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2021-11-09 21:13:29 -0800

    more tests

--- test.cpp
+++ test.cpp
@@ -5,36 +5,53 @@
 
 class EmptySpreadsheet : public ::testing::Test {
   public:
-    Spreadsheet* sheet;
+    Spreadsheet sheet;
     std::ostringstream ss;
     EmptySpreadsheet() {
     }
     void SetUp() {
     }
     void TearDown() {
-      sheet->clear();
-      delete sheet;
+      sheet.clear();
     }
 };
 
-class OnlyColumnNamesSpreadsheet : public ::testing::Test {
-  public:
-    Spreadsheet* sheet;
-    std::ostringstream ss;
-    OnlyColumnNamesSpreadsheet(){
-      sheet->set_column_names({"First","Last","Age","Major"});
-    }
-    void SetUp() {
-    }
-    void TearDown() {
-      sheet->clear();
-      delete sheet;
-    }
-};
+// class OnlyColumnNamesSpreadsheet : public ::testing::Test {
+//   public:
+//     Spreadsheet* sheet;
+//     std::ostringstream ss;
+//     OnlyColumnNamesSpreadsheet(){
+//       sheet->set_column_names({"First","Last","Age","Major"});
+//     }
+//     void SetUp() {
+//     }
+//     void TearDown() {
+//       sheet->clear();
+//       delete sheet;
+//     }
+// };
 
 TEST_F(EmptySpreadsheet, SelectContainsTest) {
-  sheet->set_selection(new Select_Contains(sheet,"Last","Dole"));
-  sheet->print_selection(ss);
+  sheet.set_selection(new Select_Contains(&sheet,"Last","Dole"));
+  sheet.print_selection(ss);
+  ASSERT_EQ(ss.str(), "");
+}
+
+TEST_F(EmptySpreadsheet, NotContaintsTest) {
+  sheet.set_selection(new Select_Not(new Select_Contains(&sheet,"Last","Dole")));
+  sheet.print_selection(ss);
+  ASSERT_EQ(ss.str(), "");
+}
+
+TEST_F(EmptySpreadsheet, SelectContainsTest) {
+  sheet.set_selection(new Select_Contains(&sheet,"Last","Dole"));
+  sheet.print_selection(ss);
+  ASSERT_EQ(ss.str(), "");
+}
+
+TEST_F(EmptySpreadsheet, NotContaintsTest) {
+  sheet.set_selection(new Select_Not(new Select_Contains(&sheet,"Last","Dole")));
+  sheet.print_selection(ss);
   ASSERT_EQ(ss.str(), "");
 }
 
