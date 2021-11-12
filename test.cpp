1d82890ce239324191ab80d7bd82529949bcfb45
Vinz Angelo Madrigal <74069694+mvinzangelo@users.noreply.github.com>
2021-11-11 23:01:43 -0800

    test.cpp uncommented out code

--- test.cpp
+++ test.cpp
@@ -16,20 +16,20 @@ class EmptySpreadsheet : public ::testing::Test {
     }
 };
 
-// class OnlyColumnNamesSpreadsheet : public ::testing::Test {
-//   public:
-//     Spreadsheet* sheet;
-//     std::ostringstream ss;
-//     OnlyColumnNamesSpreadsheet(){
-//       sheet->set_column_names({"First","Last","Age","Major"});
-//     }
-//     void SetUp() {
-//     }
-//     void TearDown() {
-//       sheet->clear();
-//       delete sheet;
-//     }
-// };
+ class OnlyColumnNamesSpreadsheet : public ::testing::Test {
+   public:
+     Spreadsheet* sheet;
+     std::ostringstream ss;
+     OnlyColumnNamesSpreadsheet(){
+       sheet->set_column_names({"First","Last","Age","Major"});
+     }
+     void SetUp() {
+     }
+     void TearDown() {
+       sheet->clear();
+       delete sheet;
+     }
+ };
 
 TEST_F(EmptySpreadsheet, SelectContainsTest) {
   sheet.set_selection(new Select_Contains(&sheet,"Last","Dole"));
